6fd48c263bc0de81f864a876b2717767873ff717
Suren Mouchsiadis <smouchsiadis@gmail.com>
2022-05-26 21:44:14 +0100

    path adjustments

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -44,94 +44,14 @@ import { AdminDashboardComponent } from "./views/adminDashboard/adminDashboard.c
     imports: [
         RouterModule.forRoot(
             [
-                {
-                    path: "",
-                    component: AppMainComponent,
-                    children: [
-                        { path: "", component: DashboardDemoComponent },
-                        {
-                            path: "uikit/formlayout",
-                            component: FormLayoutDemoComponent,
-                        },
-                        { path: "uikit/input", component: InputDemoComponent },
-                        {
-                            path: "uikit/floatlabel",
-                            component: FloatLabelDemoComponent,
-                        },
-                        {
-                            path: "uikit/invalidstate",
-                            component: InvalidStateDemoComponent,
-                        },
-                        {
-                            path: "uikit/button",
-                            component: ButtonDemoComponent,
-                        },
-                        { path: "uikit/table", component: TableDemoComponent },
-                        { path: "uikit/list", component: ListDemoComponent },
-                        { path: "uikit/tree", component: TreeDemoComponent },
-                        { path: "uikit/panel", component: PanelsDemoComponent },
-                        {
-                            path: "uikit/overlay",
-                            component: OverlaysDemoComponent,
-                        },
-                        { path: "uikit/media", component: MediaDemoComponent },
-                        { path: "uikit/menu", component: MenusDemoComponent },
-                        {
-                            path: "uikit/message",
-                            component: MessagesDemoComponent,
-                        },
-                        { path: "uikit/misc", component: MiscDemoComponent },
-                        {
-                            path: "uikit/charts",
-                            component: ChartsDemoComponent,
-                        },
-                        { path: "uikit/file", component: FileDemoComponent },
-                        {
-                            path: "utilities/display",
-                            component: DisplayComponent,
-                        },
-                        {
-                            path: "utilities/elevation",
-                            component: ElevationComponent,
-                        },
-                        {
-                            path: "utilities/flexbox",
-                            component: FlexboxComponent,
-                        },
-                        { path: "utilities/grid", component: GridComponent },
-                        { path: "utilities/icons", component: IconsComponent },
-                        {
-                            path: "utilities/widgets",
-                            component: WidgetsComponent,
-                        },
-                        {
-                            path: "utilities/spacing",
-                            component: SpacingComponent,
-                        },
-                        {
-                            path: "utilities/typography",
-                            component: TypographyComponent,
-                        },
-                        { path: "utilities/text", component: TextComponent },
-                        { path: "pages/crud", component: AppCrudComponent },
-                        {
-                            path: "pages/calendar",
-                            component: AppCalendarComponent,
-                        },
-                        {
-                            path: "pages/timeline",
-                            component: AppTimelineDemoComponent,
-                        },
-                        { path: "pages/empty", component: EmptyDemoComponent },
-                        {
-                            path: "documentation",
-                            component: DocumentationComponent,
-                        },
-                    ],
-                },
-                { path: "error", component: AppErrorComponent },
-                { path: "accessdenied", component: AppAccessdeniedComponent },
-                { path: "404", component: AppNotfoundComponent },
+                // {
+                //     path: "",
+                //     component: AppMainComponent,
+                //     children: [],
+                // },
+                //{ path: "error", component: AppErrorComponent },
+                //{ path: "accessdenied", component: AppAccessdeniedComponent },
+                //{ path: "404", component: AppNotfoundComponent },
                 { path: "login", component: AppLoginComponent },
                 {
                     path: "admin-dashboard/:id",
